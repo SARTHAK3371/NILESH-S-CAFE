0bed1278c8b2df426ed2666bce1802ed8cacbedc
SARTHAK3371 <sarthakwalunj6@gmail.com>
2026-09-10 21:43:09 +0530

    Update app.py

--- cafe_management/app.py
+++ cafe_management/app.py
@@ -32,16 +32,16 @@ login_manager.login_message_category = 'info'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Admin user create / update
-admin = User.query.filter_by(username='SARTHAKWALUNJ').first()
-if not admin:
-    admin = User(username='SARTHAKWALUNJ', name='SARTHAK WALUNJ', role='admin')
-    admin.set_password('3371')
-    db.session.add(admin)
-else:
-    admin.set_password('3371')
-    admin.name = 'SARTHAK WALUNJ'
-db.session.commit()
+        # Default admin - SARTHAK WALUNJ
+        admin = User.query.filter_by(username='SARTHAKWALUNJ').first()
+        if not admin:
+            admin = User(username='SARTHAKWALUNJ', name='SARTHAK WALUNJ', role='admin')
+            admin.set_password('3371')
+            db.session.add(admin)
+        else:
+            admin.set_password('3371')
+            admin.name = 'SARTHAK WALUNJ'
+        db.session.commit()
 
         # Sample categories
         if Category.query.count() == 0:
